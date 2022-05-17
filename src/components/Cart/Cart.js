04c0f2865566fca61f0582286195e1bcdcd6161d
Robert Muñoz Marzo <robertmunozmarzo@Roberts-MacBook-Air.local>
2022-05-17 12:10:52 +0200

    Create component CardItem and improve Cart display on Modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import StyledCart from "../styles/StyledCart";
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 
 export default function Cart({ onClose }) {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+  const cartItemRemoveHandler = () => {};
+  const cartItemAddHandler = () => {};
   const cartItems = (
-    <ul className="card-items">
-      {[{ id: "c1", name: "sushi", price: 12.99 }].map((item) => {
-        return <li>{item.name}</li>;
+    <ul
+      style={{
+        listStyle: "none",
+        margin: 0,
+        padding: 0,
+        maxHeight: "20rem",
+        overflow: "auto",
+      }}
+    >
+      {cartCtx.items.map((item) => {
+        return (
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+        );
       })}
     </ul>
   );
@@ -16,13 +41,13 @@ export default function Cart({ onClose }) {
         {cartItems}
         <div className="total">
           <span>Total amount</span>
-          <span> 35.62</span>
+          <span>{totalAmount}</span>
         </div>
         <div className="actions">
           <button className="button--alt" onClick={onClose}>
             Close
           </button>
-          <button className="button">Order</button>
+          {hasItems && <button className="button">Order</button>}
         </div>
       </StyledCart>
     </Modal>
