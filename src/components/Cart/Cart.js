475cec1a7d83071a842d23baa2e47d09800ba2d4
Robert Muñoz Marzo <robertmunozmarzo@Roberts-MacBook-Air.local>
2022-06-17 18:49:37 +0200

    Handle cart after submitting order

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,6 +8,8 @@ import Checkout from "./Checkout";
 export default function Cart({ onClose }) {
   const cartCtx = useContext(CartContext);
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setdidSubmit] = useState(false);
 
   const totalAmount = `${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
@@ -23,14 +25,18 @@ export default function Cart({ onClose }) {
     setIsCheckout(true);
   };
 
-  const submitOrderHandler = (userData) => {
-    fetch("https://react-my-burger-446ac.firebaseio.com/orders.json", {
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch("https://react-my-burger-446ac.firebaseio.com/orders.json", {
       method: "POST",
       body: JSON.stringify({
         user: userData,
         orderedItems: cartCtx.items,
       }),
     });
+    setIsSubmitting(false);
+    setdidSubmit(true);
+    cartCtx.clearCart();
   };
   const cartItems = (
     <ul className="cart-items">
@@ -61,19 +67,36 @@ export default function Cart({ onClose }) {
       )}
     </div>
   );
+  const cartModalContent = (
+    <StyledCart>
+      {cartItems}
+      <div className="total">
+        <span>Total amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={onClose} />
+      )}
+      {!isCheckout && modalActions}
+    </StyledCart>
+  );
+  const isSubmittingModalContent = <p>Sending order data...</p>;
+  const didSubmittingModalContent = (
+    <StyledCart>
+      <p>Successfully sent the order!</p>
+      <div className="actions">
+        <button className="button" onClick={onClose}>
+          Close
+        </button>
+      </div>
+    </StyledCart>
+  );
+
   return (
     <Modal onClose={onClose}>
-      <StyledCart>
-        {cartItems}
-        <div className="total">
-          <span>Total amount</span>
-          <span>{totalAmount}</span>
-        </div>
-        {isCheckout && (
-          <Checkout onConfirm={submitOrderHandler} onCancel={onClose} />
-        )}
-        {!isCheckout && modalActions}
-      </StyledCart>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmittingModalContent}
     </Modal>
   );
 }
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -53,6 +53,11 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -68,11 +73,16 @@ export default function CartProvider(props) {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
